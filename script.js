6630534ebe97fbaa1baf5e2ff307d967202ee551
alihxn <91802177+alyhxn@users.noreply.github.com>
2024-08-24 18:36:26 +0500

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+let elx
+const data = []
+const paths = document.querySelectorAll('path')
+const rects = document.querySelectorAll('rect')
+const input = document.querySelector('textarea')
+const add = document.querySelector('button')
+const history = document.querySelector('.history')
+const cards = document.querySelector('.cards')
+const submit = document.querySelector('.submit')
+
+
+paths.forEach(click)
+rects.forEach(click)
+add.onclick = onadd
+submit.onclick
+
+function click (el) {
+  el.onclick = () => {
+    el.classList.toggle('on')
+    elx = el
+    input.disabled = false
+    input.focus()
+    input.placeholder = 'Enter symptoms here'
+  }
+}
+
+function onadd () {
+  if(!data.length)
+    cards.innerHTML = ''
+  data.push(input.value)
+  const el = document.createElement('div')
+  el.classList.add('card')
+  el.innerHTML = `
+    <h2>${elx.id}</h2>
+    <div>${input.value}</div>
+  `
+  cards.append(el)
+  elx.onclick = null
+  input.value = ''
+  input.disabled = true
+  input.placeholder = 'Pick a body part to add symptoms'
+}
+
+function onsubmit () {
+  console.log('send email!')
+}
